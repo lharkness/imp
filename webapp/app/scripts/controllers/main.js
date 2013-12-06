90a6323dcc340da2906a6e4022d0927176f3936a
Lee <lee@leeharkness.com>
2013-12-05 21:27:58 -0600

    initial commit of webapp

---
+++ webapp/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('impApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
